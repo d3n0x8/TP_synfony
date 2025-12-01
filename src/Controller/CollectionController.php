5b3ebb4c968abf390a1c7e6bc3b324d4136b7cb9
Andreas Rey Malissein <andreasrey87@gmail.com>
2025-12-01 17:33:19 +0100

    Add breadcrumb menu to collection index and show templates; update h1 styles

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -7,24 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Utilisateur;
 use App\Repository\UtilisateurRepository;
+use App\Menu\MenuBuilder;
 
 final class CollectionController extends AbstractController
 {
 
     #[Route('/collection', name: 'app_collection_index')]
-    public function index(UtilisateurRepository $utilisateurRepository): Response
+    public function index(UtilisateurRepository $utilisateurRepository, MenuBuilder $menuBuilder): Response
     {
+        $breadcrumb = $menuBuilder->createBreadcrumbMenu([]);
+        $breadcrumb->addChild('Collections de jeux vidéo', ['route' => 'app_collection_index']);
+
         return $this->render('collection/index.html.twig', [
             'utilisateurs' => $utilisateurRepository->findAll(),
+            'breadcrumb' => $breadcrumb,
         ]);
     }
 
     #[Route('/collection/utilisateur/{id}', name: 'app_collection_show')]
-    public function show(Utilisateur $utilisateur): Response
+    public function show(Utilisateur $utilisateur, MenuBuilder $menuBuilder): Response
     {
+        $breadcrumb = $menuBuilder->createBreadcrumbMenu([]);
+        $breadcrumb->addChild('Collections de jeux vidéo', ['route' => 'app_collection_index']);
+        $breadcrumb->addChild('Collection de ' . $utilisateur->getPseudo());
         return $this->render('collection/show.html.twig', [
             'utilisateur' => $utilisateur,
             'collects' => $utilisateur->getCollects(),
+            'breadcrumb' => $breadcrumb,
         ]);
     }
 }
