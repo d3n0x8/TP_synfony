cf2f883e7476ca83537a2fb52a94567fc945ed58
Joachim Fevre <joachim.fevre@gmail.com>
2025-12-10 16:02:01 +0100

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -34,8 +34,155 @@ class AppFixtures extends Fixture
         $jeuVideo->setDateSortie(new \DateTime('2020-06-19'));
         $jeuVideo->setPrix(59.99);
         $manager->persist($jeuVideo);
-	$manager->flush();
         // ...
+
+        $gAction = new Genre();
+        $gAction->setNom('Action');
+        $gAction->setDescription("Jeux d'action: jeux de plateforme, combat, tir (F¨S, TPS...)");
+        $gAction->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gAction);
+
+        $gAventure = new Genre();
+        $gAventure->setNom('Aventure');
+        $gAventure->setDescription("Jeux d'aventure narrative, point and click...");
+        $gAventure->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gAventure);
+
+        $gActionAventure = new Genre();
+        $gActionAventure->setNom('Action Aventure');
+        $gActionAventure->setDescription(": (Infiltration, survival, …)");
+        $gActionAventure->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gActionAventure);
+
+        $gRpg = new Genre();
+        $gRpg->setNom('RPG');
+        $gRpg->setDescription(" Jeux de rôle, MMORPG, …");
+        $gRpg->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gRpg);
+
+        $gStategie = new Genre();
+        $gStategie->setNom('Strategie');
+        $gStategie->setDescription(" : Jeux de stratégie (RTS, turn-based)");
+        $gStategie->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gStategie);
+
+        $gSimulation = new Genre();
+        $gSimulation->setNom('Simulation');
+        $gSimulation->setDescription(" : Jeux de simulation, de gestion");
+        $gSimulation->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gSimulation);
+
+        $gSport = new Genre();
+        $gSport->setNom('Sport');
+        $gSport->setDescription("Jeux de sport");
+        $gSport->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gSport);
+
+        $gCourse = new Genre();
+        $gCourse->setNom('Course');
+        $gCourse->setDescription(": jeux de course par ex. automobil");
+        $gCourse->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gCourse);
+
+        $gReflexion = new Genre();
+        $gReflexion->setNom('Reflexion');
+        $gReflexion->setDescription(" : Jeux de réflexion, puzzles, casse-tête");
+        $gReflexion->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($gReflexion);
+
+
+
+        $eNintendo = new Editeur();
+        $eNintendo->setNom('Nintendo');
+        $eNintendo->setPays('Japon');
+        $eNintendo->setSiteWeb('https://www.nintendo.com');
+        $eNintendo->setCreatedAt(new \DateTimeImmutable('now'));
+        $manager->persist($eNintendo);
+
+
+
+
+        $smash = new JeuVideo();
+        $smash->setTitre('Super Smash Bros Ultimate');
+        $smash->setEditeur($eNintendo);
+        $smash->setGenre($gAction);
+        $smash->setDeveloppeur('Nintendo EAD Tokyo');
+        $smash->setDateSortie(new \DateTime('2018-12-07'));
+        $smash->setPrix(59.99);
+        $manager->persist($smash);
+
+
+        $zelda = new JeuVideo();
+        $zelda->setTitre('The Legend of Zelda: The Wind Waker');
+        $zelda->setEditeur($eNintendo);
+        $zelda->setGenre($gAventure);
+        $zelda->setDeveloppeur('Nintendo EAD Tokyo');
+        $zelda->setDateSortie(new \DateTime('2002-12-13'));
+        $zelda->setPrix(39.99);
+        $manager->persist($zelda);
+
+
+        $xeno = new JeuVideo();
+        $xeno->setTitre('Xenoblade Chronicles');
+        $xeno->setEditeur($eNintendo);
+        $xeno->setGenre($gRpg);
+        $xeno->setDeveloppeur('Monolith Soft');
+        $xeno->setDateSortie(new \DateTime('2010-06-10'));
+        $xeno->setPrix(39.99);
+        $manager->persist($xeno);
+
+
+        $fireemblem = new JeuVideo();
+        $fireemblem->setTitre('Fire Embleme: Three Houses');
+        $fireemblem->setEditeur($eNintendo);
+        $fireemblem->setGenre($gStategie);
+        $fireemblem->setDeveloppeur('Intelligent Systems');
+        $fireemblem->setDateSortie(new \DateTime('2019-07-26'));
+        $fireemblem->setPrix(59.99);
+        $manager->persist($fireemblem);
+
+
+        $animal = new JeuVideo();
+        $animal->setTitre('Animal Crossing: New Leaf');
+        $animal->setEditeur($eNintendo);
+        $animal->setGenre($gSimulation);
+        $animal->setDeveloppeur('Nintendo EAD');
+        $animal->setDateSortie(new \DateTime('2012-11-08'));
+        $animal->setPrix(24.99);
+        $manager->persist($animal);
+
+
+        $wiifit = new JeuVideo();
+        $wiifit->setTitre('Wii Fit');
+        $wiifit->setEditeur($eNintendo);
+        $wiifit->setGenre($gSport);
+        $wiifit->setDeveloppeur('Nintendo EAD');
+        $wiifit->setDateSortie(new \DateTime('2007-12-01'));
+        $wiifit->setPrix(24.99);
+        $manager->persist($wiifit);
+
+
+        $mkworld = new JeuVideo();
+        $mkworld->setTitre('Mario Kart World');
+        $mkworld->setEditeur($eNintendo);
+        $mkworld->setGenre($gCourse);
+        $mkworld->setDeveloppeur('Nintendo EPD');
+        $mkworld->setDateSortie(new \DateTime('2025-06-05'));
+        $mkworld->setPrix(79.99);
+        $manager->persist($mkworld);
+
+
+        $toad = new JeuVideo();
+        $toad->setTitre('Captain Toad Treasure Tracker');
+        $toad->setEditeur($eNintendo);
+        $toad->setGenre($gReflexion);
+        $toad->setDeveloppeur('Nintendo EAD Tokyo');
+        $toad->setDateSortie(new \DateTime('2014-11-13'));
+        $toad->setPrix(39.99);
+        $manager->persist($toad);
+
+        $manager->flush();
+        
 
 
 // Création d'un utilisateur
